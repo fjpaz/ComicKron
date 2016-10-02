d968af0015588cb78068d5d4f9704c5af3067203
Francisco Javier Paz <fcojpaz@gmail.com>
2016-10-02 09:14:26 +0200

    Mantain page aspect ratio and fit to available space

--- src/KronUI/items/ImageShowcase.cpp
+++ src/KronUI/items/ImageShowcase.cpp
@@ -24,7 +24,19 @@ QVariant ImageShowcase::image() const
 
 void ImageShowcase::paint(QPainter* painter)
 {
-    painter->drawImage(contentsBoundingRect(), image_);
+    qreal aspectRatio = static_cast<qreal>(image_.width()) / image_.height();
+    qreal paintWidth = height() * aspectRatio;
+
+    if (paintWidth <= width())
+    {
+        painter->drawImage(QRectF(x() + width() / 2.0 - paintWidth / 2.0, y(), paintWidth, height()), image_);
+    }
+    else
+    {
+        qreal paintHeight = width() / aspectRatio;
+
+        painter->drawImage(QRectF(x(), y() + height() / 2.0 - paintHeight / 2.0, width(), paintHeight), image_);
+    }
 }
 
 void ImageShowcase::setImage(QVariant image)
